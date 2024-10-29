6c06d05f4af24dbb9cc42f9f73ccfdb5c31b3d8f
2382019 <2382019@unai.edu>
2024-10-29 15:59:35 +0700

    handson7

--- Polimorfisme/Main.java
+++ Polimorfisme/Main.java
@@ -6,5 +6,13 @@ public class Main {
         bangunDatar = new Persegi(4);
         System.out.println("Luas Persegi : " + bangunDatar.luas());
         System.out.println("Keliling Persegi : " + bangunDatar.keliling());
+
+        bangunDatar = new Lingkaran(7); // Misalkan jari-jari lingkaran adalah 7
+        System.out.println("Luas Lingkaran : " + bangunDatar.luas());
+        System.out.println("Keliling Lingkaran : " + bangunDatar.keliling());
+
+        bangunDatar = new SegitigaSamaSisi(5); // Misalkan sisi segitiga adalah 5
+        System.out.println("Luas Segitiga Sama Sisi : " + bangunDatar.luas());
+        System.out.println("Keliling Segitiga Sama Sisi : " + bangunDatar.keliling());
     }
 }
